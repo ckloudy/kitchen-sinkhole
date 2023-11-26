771b2f7bcf4ef5117724408d3e50bd2c783f461b
MonoliYoda <48124492+MonoliYoda@users.noreply.github.com>
2023-11-26 22:30:57 +0100

    Fixed a typo

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ const navbar = () => {
             <div className="flex align-middle">
                 <img src="Images/Gator-Logo.svg" alt="logo" width={34} height={34} />
                 <a href="/" className="flex pl-1">
-                    <p className="py-2 pl-1 text-md md:text-[1.3rem]">Kitcken Sinkhole</p>
+                    <p className="py-2 pl-1 text-md md:text-[1.3rem]">Kitchen Sinkhole</p>
                 </a>
             </div>
             <p className="p-2 text-md md:text-[1.3rem] tracking-wide jockey">Welcome Capsuleer!</p>
